0419a14d9d4453bcda461b7c35f7eb77f06eac36
suetsugu <y.suetsugu@shaft.bz>
2023-12-15 18:27:19 +0900

    mod: transition setting

--- apps/client/src/app/app.config.ts
+++ apps/client/src/app/app.config.ts
@@ -1,7 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter, withViewTransitions } from '@angular/router';
+import { ApplicationConfig, inject } from '@angular/core';
+import { IsActiveMatchOptions, Router, provideRouter, withViewTransitions } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withViewTransitions())],
+  providers: [provideRouter(appRoutes, withViewTransitions({
+    onViewTransitionCreated: ({ transition }) => {
+      const router = inject(Router)
+      const targetUrl = router.getCurrentNavigation()!.finalUrl!
+
+      // fragmentとqueryParamsの変更は無視する（transitionを行わない）
+      const config: IsActiveMatchOptions = {
+        paths: 'exact',
+        matrixParams: 'exact',
+        fragment: 'ignored',
+        queryParams: 'ignored',
+      }
+      if (router.isActive(targetUrl, config)) {
+        transition.skipTransition()
+      }
+    },
+  }))],
 };
