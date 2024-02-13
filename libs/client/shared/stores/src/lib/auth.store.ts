34b5136df3c7400a6ee17122d9c1b4e4276a72b5
suetsugu <y.suetsugu@shaft.bz>
2024-02-13 18:44:42 +0900

    store

--- libs/client/shared/stores/src/lib/auth.store.ts
+++ libs/client/shared/stores/src/lib/auth.store.ts
@@ -1,19 +1,37 @@
-import { inject } from '@angular/core';
-import { signalStore, withMethods, withState } from '@ngrx/signals';
+import { computed, inject } from '@angular/core';
+import { signalStore, withComputed, withHooks, withMethods, withState } from '@ngrx/signals';
 import { AuthService } from '@nx-demo/client-shared-services';
 
 export const AuthStore = signalStore(
+  { providedIn: 'root' },
+
   withState({
     isLoggedIn: false,
   }),
+
   withMethods(state => {
+    const { isLoggedIn } = state;
     const authService = inject(AuthService);
 
     return {
       /** ログイン */
-      login(email: string, password: string) {
+      login: (email: string, password: string) => {
+        isLoggedIn()
         return authService.login(email, password);
       },
     }
+  }),
+
+  withComputed(state => {
+    const { isLoggedIn } = state;
+
+    return {
+      // selected: computed(() => flights().filter((f) => basket()[f.id])),
+      // criteria: computed(() => ({ from: from(), to: to() })),
+    }
+  }),
+
+  withHooks(() => {
+    
   }),
 );
