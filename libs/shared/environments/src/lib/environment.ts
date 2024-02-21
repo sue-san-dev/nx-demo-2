1ba09569c94c24011abcebfe31ce69e9169f9c1e
otani4 <k.otani@shaft.bz>
2024-02-21 19:55:34 +0900

    environment修正

--- libs/shared/environments/src/lib/environment.ts
+++ libs/shared/environments/src/lib/environment.ts
@@ -2,5 +2,5 @@ import { IEnvironment } from './environment.interface';
 
 export const environment: IEnvironment = {
   production: false,
-  apiUrl: 'https://nx-demo-2-api.onrender.com',
+  apiUrl: 'https://nx-demo-2-api.onrender.com/api/v1',
 };
