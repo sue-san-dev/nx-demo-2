ba8fd07ada8e892f21df535cd9ee4b2b5a8bcb58
otani4 <k.otani@shaft.bz>
2024-02-24 11:00:30 +0900

    auth()修正

--- libs/client/shared/services/src/lib/auth.service.ts
+++ libs/client/shared/services/src/lib/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { ILoginPayload, IUser } from '@nx-demo/shared-domain';
 import { ReqUrlUtil } from '@nx-demo/shared-utils';
-import { Observable, map, shareReplay, tap } from 'rxjs';
+import { Observable, finalize, map, shareReplay } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
@@ -17,7 +17,7 @@ export class AuthService {
     if (this.#auth$ == null) {
       this.#auth$ = this.#http.post<IUser | null>(ReqUrlUtil.auth.root, null).pipe(
         shareReplay(1),
-        tap(() => this.#auth$ = null),
+        finalize(() => this.#auth$ = null),
       );
     }
     return this.#auth$;
