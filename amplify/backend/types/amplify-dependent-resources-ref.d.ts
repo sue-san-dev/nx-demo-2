4f0fe194acd52b48b8551c992ab4f43b22b84961
otani4 <k.otani@shaft.bz>
2024-03-24 20:08:34 +0900

    プッシュ

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "apie2b4c029": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "function": {
     "nxDemo2": {
       "Arn": "string",
