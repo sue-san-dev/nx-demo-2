267a9e86ecb85d6adc7080ab3c90288d5fdf7c13
otani4 <k.otani@shaft.bz>
2024-03-25 00:44:26 +0900

    amplify:nxDemo2変更

--- amplify/backend/function/nxDemo2/src/index.js
+++ amplify/backend/function/nxDemo2/src/index.js
@@ -1,28 +1,33 @@
-import { __awaiter } from "tslib";
-import { createServer, proxy } from 'aws-serverless-express';
-import { eventContext } from 'aws-serverless-express/middleware';
-import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import { AppModule } from './apps/api/src/app/app.module';
-import * as express from 'express';
-import { config } from 'aws-sdk';
+require('module-alias/register');
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.handler = void 0;
+const tslib_1 = require("tslib");
+const aws_serverless_express_1 = require("aws-serverless-express");
+const middleware_1 = require("aws-serverless-express/middleware");
+const core_1 = require("@nestjs/core");
+const platform_express_1 = require("@nestjs/platform-express");
+const app_module_1 = require("./apps/api/src/app/app.module");
+const express = require("express");
+const aws_sdk_1 = require("aws-sdk");
 const binaryMimeTypes = [];
 let cachedServer;
 function bootstrapServer() {
-    return __awaiter(this, void 0, void 0, function* () {
+    return tslib_1.__awaiter(this, void 0, void 0, function* () {
         if (!cachedServer) {
             const expressApp = express();
-            const nestApp = yield NestFactory.create(AppModule, new ExpressAdapter(expressApp));
-            nestApp.use(eventContext());
-            config.update({ region: process.env.REGION });
+            const nestApp = yield core_1.NestFactory.create(app_module_1.AppModule, new platform_express_1.ExpressAdapter(expressApp));
+            nestApp.use((0, middleware_1.eventContext)());
+            aws_sdk_1.config.update({ region: process.env.REGION });
             yield nestApp.init();
-            cachedServer = createServer(expressApp, undefined, binaryMimeTypes);
+            cachedServer = (0, aws_serverless_express_1.createServer)(expressApp, undefined, binaryMimeTypes);
         }
         return cachedServer;
     });
 }
-export const handler = (event, context) => __awaiter(void 0, void 0, void 0, function* () {
+const handler = (event, context) => tslib_1.__awaiter(void 0, void 0, void 0, function* () {
     cachedServer = yield bootstrapServer();
-    return proxy(cachedServer, event, context, 'PROMISE').promise;
+    return (0, aws_serverless_express_1.proxy)(cachedServer, event, context, 'PROMISE').promise;
 });
+exports.handler = handler;
 //# sourceMappingURL=index.js.map
--- amplify/backend/function/nxDemo2/src/jest.config.js
+++ amplify/backend/function/nxDemo2/src/jest.config.js
@@ -1,5 +1,7 @@
-import { getJestProjects } from '@nx/jest';
-export default {
-    projects: getJestProjects(),
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jest_1 = require("@nx/jest");
+exports.default = {
+    projects: (0, jest_1.getJestProjects)(),
 };
 //# sourceMappingURL=jest.config.js.map
